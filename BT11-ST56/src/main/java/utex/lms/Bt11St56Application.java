aad365cd5227462ad0e47670ac3b224641bd68e9
phanttuan <tuanphan19405@gmail.com>
2025-10-03 09:04:07 +0700

    Hoan thanh demo 1 - SpringSecurity6

---
+++ BT11-ST56/src/main/java/utex/lms/Bt11St56Application.java
@@ -0,0 +1,13 @@
+package utex.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt11St56Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bt11St56Application.class, args);
+	}
+
+}
---
+++ BT11-ST56/src/test/java/utex/lms/Bt11St56ApplicationTests.java
@@ -0,0 +1,13 @@
+package utex.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bt11St56ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
